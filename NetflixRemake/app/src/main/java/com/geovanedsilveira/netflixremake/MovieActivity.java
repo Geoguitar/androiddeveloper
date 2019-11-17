c6769e68cbeec8007c19b401620b50979750f1c4
Geoguitar <geovanedsilveira@gmail.com>
2019-11-17 03:40:30 -0300

    alterado layout

--- NetflixRemake/app/src/main/java/com/geovanedsilveira/netflixremake/MovieActivity.java
+++ NetflixRemake/app/src/main/java/com/geovanedsilveira/netflixremake/MovieActivity.java
@@ -2,8 +2,12 @@ package com.geovanedsilveira.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -18,6 +22,13 @@ public class MovieActivity extends AppCompatActivity {
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+        }
+
+          LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+        if (drawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4);
+            drawable.setDrawableByLayerId(R.id.cover_drawble, movieCover);
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
         }
     }
 }
