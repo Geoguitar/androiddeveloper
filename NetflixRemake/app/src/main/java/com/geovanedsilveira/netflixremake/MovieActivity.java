3ddd41288e51a14e5d87e6026d749f2dded80204
Geoguitar <geovanedsilveira@gmail.com>
2019-11-21 00:59:22 -0300

    Descrição e elenco p/Moke

--- NetflixRemake/app/src/main/java/com/geovanedsilveira/netflixremake/MovieActivity.java
+++ NetflixRemake/app/src/main/java/com/geovanedsilveira/netflixremake/MovieActivity.java
@@ -8,13 +8,22 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private TextView txtTitle;
+    private TextView txtDesc;
+    private TextView txtCast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        txtTitle = findViewById(R.id.text_view_title);
+        txtDesc = findViewById(R.id.text_view_desc);
+        txtCast = findViewById(R.id.text_view_cast);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -22,6 +31,7 @@ public class MovieActivity extends AppCompatActivity {
         if(getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+            getSupportActionBar().setTitle(null);
         }
 
           LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
@@ -30,5 +40,11 @@ public class MovieActivity extends AppCompatActivity {
             drawable.setDrawableByLayerId(R.id.cover_drawble, movieCover);
             ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
         }
+
+        txtTitle.setText("Batman Begins");
+        txtDesc.setText("O jovem Bruce Wayne cai em um poco e é atacado por morcegos.Bruce acorda de um pesadelo sobre" +
+                " seu passado e descobre que é prisioneiro no Butão.Ele asdfasdfasdfasdfasdf");
+        txtCast.setText(getString(R.string.cast, "Christian Bale" + ",Michael Cane" + ",Liam Neeson" +
+                ",Katie Holmes" + ",Gary Oldman"));
     }
 }
