d2bf67d13fad546a035aa1759c2fa3a3a6900e75
Geoguitar <geovanedsilveira@gmail.com>
2019-12-01 01:01:41 -0300

    view com scroll e recycler

--- NetflixRemake/app/src/main/java/com/geovanedsilveira/netflixremake/MovieActivity.java
+++ NetflixRemake/app/src/main/java/com/geovanedsilveira/netflixremake/MovieActivity.java
@@ -1,20 +1,31 @@
 package com.geovanedsilveira.netflixremake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.geovanedsilveira.netflixremake.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieActivity extends AppCompatActivity {
 
     private TextView txtTitle;
     private TextView txtDesc;
     private TextView txtCast;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class MovieActivity extends AppCompatActivity {
         txtTitle = findViewById(R.id.text_view_title);
         txtDesc = findViewById(R.id.text_view_desc);
         txtCast = findViewById(R.id.text_view_cast);
+        recyclerView = findViewById(R.id.recycler_view_similar);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,5 +58,56 @@ public class MovieActivity extends AppCompatActivity {
                 " seu passado e descobre que é prisioneiro no Butão.Ele asdfasdfasdfasdfasdf");
         txtCast.setText(getString(R.string.cast, "Christian Bale" + ",Michael Cane" + ",Liam Neeson" +
                 ",Katie Holmes" + ",Gary Oldman"));
+
+        List<Movie> movies =  new ArrayList<>();
+        for (int i = 0; i <30; i++) {
+            Movie movie = new Movie();
+            movies.add(movie);
+        }
+
+        recyclerView.setAdapter(new MovieAdapter(movies));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
     }
+
+    private static class MovieHolder extends RecyclerView.ViewHolder {
+
+        final ImageView imageViewCover;
+
+        public MovieHolder(@NonNull View itemView){
+            super(itemView);
+            imageViewCover = itemView.findViewById(R.id.image_view_cover);
+        }
+
+    }
+
+    private class MovieAdapter extends RecyclerView.Adapter<MovieHolder> {
+
+        private final List<Movie> movies;
+
+        private MovieAdapter(List<Movie> movies){
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MovieHolder(getLayoutInflater()
+                    .inflate(R.layout.movie_item_similar, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MovieHolder holder, int position) {
+            Movie movie = movies.get(position);
+
+        }
+
+        @Override
+        public int getItemCount() {
+
+            return movies.size();
+        }
+
+    }
+
 }
+
